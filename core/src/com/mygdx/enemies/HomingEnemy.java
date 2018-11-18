c012b9e805cf9a5543a7df9ea5349628139951ce
PhaseRush <leozhang888@yahoo.ca>
2018-11-18 11:54:02 -0800

    Cleaned up starfighter - switch extract dupe + position updating for rect rep

---
+++ core/src/com/mygdx/enemies/HomingEnemy.java
@@ -0,0 +1,10 @@
+package com.mygdx.enemies;
+
+import com.mygdx.hellinspace.GameMain;
+
+public class HomingEnemy extends Enemy{
+
+    public HomingEnemy(GameMain game) {
+        super(game);
+    }
+}
--- core/src/com/mygdx/projectiles/HomingBullet.java
+++ core/src/com/mygdx/projectiles/HomingBullet.java
@@ -1,19 +1,23 @@
 package com.mygdx.projectiles;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 import com.mygdx.hellinspace.GameMain;
 
 public class HomingBullet extends Bullet {
     Vector2 target;
-
     public HomingBullet(GameMain game, float x, float y, float vx, float vy, boolean isGood, float bearing, float damageValue, Vector2 target) {
         super(game, x, y, vx, vy, isGood, bearing, damageValue);
         this.target = target;
+        bulletTexture = new Texture(Gdx.files.internal("Projectiles\\redlaser.png"));
     }
 
     @Override
     public void move(float delta) {
-        super.move(delta);
+        if (target == null) return;
+
+        super.move(delta); //todo
     }
 
     public Vector2 getTarget () {
